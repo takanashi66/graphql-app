a604689ef570ebf25dd83b758ef9f16d720a6b94
t.inoue <t.inoue@funaffect.jp>
2023-09-01 14:52:48 +0900

    graphqlで見事にSyntax Errorが出るところまで

---
+++ app/GraphQL/Queries/PostsQuery.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Queries;
+
+use Closure;
+use GraphQL\Type\Definition\ResolveInfo;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Query;
+use Rebing\GraphQL\Support\SelectFields;
+
+class PostsQuery extends Query
+{
+    protected $attributes = [
+        'name' => 'posts',
+        'description' => 'A query',
+        'model' => Posts::class,
+    ];
+
+    public function type(): Type
+    {
+        return Type::listOf(Type::string());
+    }
+
+    public function args(): array
+    {
+        return [
+            'id' => [
+                'name' => 'id',
+                'type' => Type::string(),
+            ],
+            'title' => [
+                'name' => 'title',
+                'type' => Type::string(),
+            ]
+        ];
+    }
+
+    public function resolve($root, array $args, $context, ResolveInfo $resolveInfo, Closure $getSelectFields)
+    {
+        // /** @var SelectFields $fields */
+        // $fields = $getSelectFields();
+        // $select = $fields->getSelect();
+        // $with = $fields->getRelations();
+
+        // return [
+        //     'The posts works',
+        // ];
+        if (isset($args['id'])) {
+            return User::where('id' , $args['id'])->get();
+        }
+
+        if (isset($args['title'])) {
+            return User::where('title', $args['title'])->get();
+        }
+
+        return User::all();
+    }
+}
